0ddb1f4330e98a843bc2a10eecc1d10dd3ba3894
Leon Slater <admin@lpslater.co.uk>
2014-08-03 14:53:38 +0100

    updating readme

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-simpleswipe
+lpswipe
 ===========
+
+lpswipe is a small (3kb when minified) standalone javascript library built to handle cross-device event listeners for touch interaction.
+
+It can be used as either a basic javascript function, or as a jQuery plugin (if jQuery is present on the page). All you have to do is specify the element(s), and the options, and lpswipe will handle all the cross-device events, including for windows phones, and touch-screen windows tablets and laptops.
+
+Why did I write lpswipe?
+-----------
+There are a lot of other touch libraries out there, and I experimented with using quite a few of them, and almost all of them have serious issues when multiple touches are used on an element. So I decided to write my own touch library that handles those issues.
+
+Multi-touch handling:
+-----------
+The multi-touch handling in lpswipe is simple - just ignore it. When browsing the web, users typically don't expect any complex touch interactions, so multi-touch handling doesn't need to be complex, we just need to make sure a second touch on the element doesn't break anything. So with lpswipe, all custom touch interactions are based completely on the first touch that is registered on the element; any additional touches will just be ignored.
+
+Usage:
+-----------
+To use lpswipe, simply call the lpswipe function and pass it the desired element(s), and an object with your defined options:
+    lpswipe(document.getElementById('foo'), {});
+or with jQuery:
+    $('#foo').lpswipe({});
+
+The available options and their defaults are:
+
+    // the distance the swipe needs to be to fire the beforeEnd and directional functions
+    threshold: 20,
+
+    // the direction to enable custom swipe gestures: 'vertical', 'horizontal', or 'all' (directional, and notReached functions will not fire if "all" is used)
+    // this is needed as it will prevent the default browser behaviour when swiping in that direction on that element
+    swipeDirection: 'horizontal',
+
+    // fires on first touch
+    start: function (d) { },
+
+    // fires on touch end event, before directional events are fired, only if threshold is reached
+    beforeEnd: function(d) { },
+
+    // move finger left to right - fires only if threshold is reached
+    right: function (d) { },
+
+    // move finger right to left - fires only if threshold is reached
+    left: function (d) { },
+
+    // move finger down to up - only if threshold is reached
+    up: function (d) { },
+
+    // move finger up to down - only if threshold is reached
+    down: function (d) { },
+
+    // fires during swipe movement
+    moving: function (d) { },
+
+    // fires if the threshold isn't reached - won't fire if all is used for the swipeDirection
+    notReached: function (d) { },
+
+    // fires on touchend in all cases, but only when user has swiped in specified direction (will fire in all cases if 'all' is used for swipeDirection)
+    end: function (d) { },
+
+    // fires when touch events are reset (e.g. on touchcancel event, or when touch interaction ends) - will always be last event to fire
+    reset: function (d) { }
+
--- js/main.js
+++ js/main.js
@@ -1,200 +1,240 @@
-/* 
- * ---------------------------------------- *
- * Name: Primary JavaScripts                *
- * Type: JavaScript                         *
- * Version: 1.0.0                           *
- * Author: Leon Slater | Codehouse LTD      *
- * ---------------------------------------- *
- */
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~             lpswipe, sidenav javascript             ~~
+~~           Leon Slater, www.lpslater.co.uk           ~~
+~~                    Version 1.0.0                    ~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
-$('html').removeClass('no-js').addClass('js');
+(function(){
 
-$(document).ready(function(){    
-    siteNav.init();
-});
+    var requestAnimFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame ||
+            window.oRequestAnimationFrame || window.msRequestAnimationFrame,
+        cancelAnimFrame = window.cancelAnimationFrame || window.webkitCancelAnimationFrame || window.mozCancelAnimationFrame ||
+            window.oCancelAnimationFrame || window.msCancelAnimationFrame;
 
-var requestAnimFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || 
-        window.oRequestAnimationFrame || window.msRequestAnimationFrame,
-    cancelAnimFrame = window.cancelAnimationFrame || window.webkitCancelAnimationFrame || window.mozCancelAnimationFrame || 
-        window.oCancelAnimationFrame || window.msCancelAnimationFrame;
+    var siteNav = {
 
-var siteNav = {
-    
-    init: function(){      
-        siteNav.storeVars();
-        siteNav.domEvents();
-        siteNav.setNavSwipe();
-        siteNav.setNavScrollOne();
-    },
-    
-    storeVars: function(){
-        siteNav.$body = $('body');
-        siteNav.$mainNav = $('#main-nav');
-        siteNav.$mobHeader = $('#mob-header');
-        siteNav.$mobOpener = siteNav.$mobHeader.find('.mobile-opener');
-        siteNav.mainNavWidth = siteNav.$mainNav.outerWidth();
-        siteNav.overrideNavScroll = false;
-    },
-    
-    setTransforms: function($elem, prop){
-        $elem.css({'-webkit-transform': prop, '-moz-transform': prop, '-o-transform': prop, 'transform': prop});
-    },
-    
-    closeNav: function(){
-        siteNav.setTransforms(siteNav.$mainNav, 'translate3d('+ siteNav.mainNavWidth +'px, 0, 0)');
-        siteNav.setTransforms(siteNav.$mobHeader, 'translate3d(0, 0, 0)');
-    },
-    
-    openNav: function(){
-        siteNav.setTransforms(siteNav.$mainNav, 'translate3d(0, 0, 0)');
-        siteNav.setTransforms(siteNav.$mobHeader, 'translate3d(-'+ siteNav.mainNavWidth +'px, 0, 0)');
-    },
-    
-    navMovementPrep: function(d){
-        var navState = siteNav.$body.hasClass('open-nav') ? 'open' : 'closed', // if nav is open or closed
-            direction = d.posX > 0 ? 'right' : 'left', // if swipe is going to the right, or left
-        movementObj = {
-            'open': { 
-                'right': function(){ 
-                    if (d.posX < siteNav.mainNavWidth){ 
-                        siteNav.navClosingMovement(d.posX, d.posX + siteNav.mainNavWidth);
-                    } else { // if user has swiped further to the right than the width of the nav
-                        siteNav.closeNav(); // use closeNav function to ensure that nav is in correct position off screen
+        init: function(){
+            siteNav.storeVars();
+            siteNav.checkCSSProp();
+            siteNav.domEvents();
+            siteNav.setNavSwipe();
+            siteNav.setNavScroll();
+            siteNav.resizeEvent();
+        },
+
+        storeVars: function(){
+            siteNav.body = document.getElementsByTagName('body')[0];
+            siteNav.mainNav = document.getElementById('main-nav');
+            siteNav.mobHeader = document.getElementById('mob-header');
+            siteNav.mobOpener = document.getElementById('mobile-opener');
+            siteNav.mainNavWidth = siteNav.mainNav.offsetWidth;
+            siteNav.overrideNavScroll = false;
+        },
+
+        checkCSSProp: function(){
+             var div = document.createElement('div'),
+                props = ['WebkitTransform', 'MozTransform', 'OTransform', 'msTransform', 'transform'];
+            for (var i in props) { // cycle through css Perspective properties to see if the browser supports them
+                if (div.style[props[i]] !== undefined) {
+                    siteNav.animProp = props[i];
+                }
+            }
+        },
+
+        setTransforms: function(elem, prop){
+            elem.style[siteNav.animProp] = prop;
+        },
+
+        closeNav: function(){
+            siteNav.setTransforms(siteNav.mainNav, 'translate3d('+ siteNav.mainNavWidth +'px, 0, 0)');
+            siteNav.setTransforms(siteNav.mobHeader, 'translate3d(0, 0, 0)');
+        },
+
+        openNav: function(){
+            siteNav.setTransforms(siteNav.mainNav, 'translate3d(0, 0, 0)');
+            siteNav.setTransforms(siteNav.mobHeader, 'translate3d(-'+ siteNav.mainNavWidth +'px, 0, 0)');
+        },
+
+        navMovementPrep: function(d){
+            var navState = siteNav.body.className.indexOf('open-nav') > -1 ? 'open' : 'closed', // if nav is open or closed
+                direction = d.posX > 0 ? 'right' : 'left', // if swipe is going to the right, or left
+            movementObj = {
+                'open': {
+                    'right': function(){
+                        if (d.posX < siteNav.mainNavWidth){
+                            siteNav.navClosingMovement(d.posX, d.posX + siteNav.mainNavWidth);
+                        } else { // if user has swiped further to the right than the width of the nav
+                            siteNav.closeNav(); // use closeNav function to ensure that nav is in correct position off screen
+                        }
+                    },
+                    'left': siteNav.openNav
+                },
+                'closed': {
+                    'right': siteNav.closeNav,
+                    'left': function(){
+                        if (d.posX <= -siteNav.mainNavWidth){ // if at or past the navwidth, force nav to stay in final open state
+                            siteNav.openNav(); // use openNav function to ensure that nav is in correct position on screen
+                        } else {
+                            siteNav.navOpeningMovement(d.posX, d.posX + siteNav.mainNavWidth);
+                        }
+                    }
+                }
+            }[navState][direction]();
+        },
+
+        navOpeningMovement: function(posX, mainNavPos){
+            siteNav.setTransforms(siteNav.mobHeader, 'translate3d('+ posX +'px, 0, 0)');
+            siteNav.setTransforms(siteNav.mainNav, 'translate3d('+ mainNavPos +'px, 0, 0)');
+        },
+
+        navClosingMovement: function(posX, mainNavPos){
+            var mobHeaderMovement = -siteNav.mainNavWidth + posX;
+            siteNav.setTransforms(siteNav.mobHeader, 'translate3d('+ mobHeaderMovement +'px, 0, 0)');
+            siteNav.setTransforms(siteNav.mainNav, 'translate3d('+ posX +'px, 0, 0)');
+        },
+
+        removeClass: function(elem, classToRemove){
+            elem.className = elem.className.replace(classToRemove, '').replace(/^\s+|\s+$/gm,'');;
+        },
+
+        addClass: function(elem, classToAdd){
+            var elemClass = elem.className;
+            if (elemClass.indexOf(classToAdd) === -1){
+                elem.className = elem.className + ' ' + classToAdd;
+            }
+        },
+
+        setNavSwipe: function(){
+            lpswipe(document.documentElement, {
+                threshold: 50,
+                swipeDirection: 'horizontal',
+                start: function(){
+                    siteNav.mainNavWidth = siteNav.mainNav.offsetWidth; // reset when opening/closing nav to ensure width is detected correctly
+                    siteNav.removeClass(siteNav.mobHeader, 'transition');
+                    siteNav.removeClass(siteNav.mainNav, 'transition');
+                },
+                beforeEnd: function(){
+                    cancelAnimFrame(animRequestId);
+                    siteNav.addClass(siteNav.mobHeader, 'transition'); // add classes here to ensure animation works on MS surface
+                    siteNav.addClass(siteNav.mainNav, 'transition');
+                },
+                left: function(){
+                    if (siteNav.body.className.indexOf('open-nav') === -1){
+                        siteNav.addClass(siteNav.body, 'open-nav');
+                        siteNav.openNav();
                     }
                 },
-                'left': siteNav.openNav 
-            },
-            'closed': { 
-                'right': siteNav.closeNav, 
-                'left': function(){ 
-                    if (d.posX <= -siteNav.mainNavWidth){ // if at or past the navwidth, force nav to stay in final open state
-                        siteNav.openNav(); // use openNav function to ensure that nav is in correct position on screen
-                    } else {
-                        siteNav.navOpeningMovement(d.posX, d.posX + siteNav.mainNavWidth);
+                right: function(){
+                    if (siteNav.body.className.indexOf('open-nav') > -1){
+                        siteNav.removeClass(siteNav.body, 'open-nav');
+                        siteNav.closeNav();
                     }
+                },
+                moving: function(d){
+                    animRequestId = requestAnimFrame(function(){
+                        siteNav.navMovementPrep(d);
+                    });
+                },
+                notReached: function(){
+                    if (siteNav.body.className.indexOf('open-nav') > -1){
+                        siteNav.openNav(); // reset to open nav position
+                    } else {
+                        siteNav.closeNav(); // reset to closed nav position
+                    }
+                },
+                reset: function(){
+                    siteNav.addClass(siteNav.mobHeader, 'transition'); // add classes here to reenable animations if user hasn't used custom swipe action
+                    siteNav.addClass(siteNav.mainNav, 'transition');
                 }
-            },
-        }[navState][direction]();
-    },
-    
-    navOpeningMovement: function(posX, mainNavPos){
-        siteNav.setTransforms(siteNav.$mobHeader, 'translate3d('+ posX +'px, 0, 0)');
-        siteNav.setTransforms(siteNav.$mainNav, 'translate3d('+ mainNavPos +'px, 0, 0)');
-    },
-    
-    navClosingMovement: function(posX, mainNavPos){
-        var mobHeaderMovement = -siteNav.mainNavWidth + posX;
-        siteNav.setTransforms(siteNav.$mobHeader, 'translate3d('+ mobHeaderMovement +'px, 0, 0)');
-        siteNav.setTransforms(siteNav.$mainNav, 'translate3d('+ posX +'px, 0, 0)');
-    },
-        
-    setNavSwipe: function(){
-        $('html').lpswipe({
-            threshold: 50,
-            swipeDirection: 'horizontal',
-            start: function(){
-                siteNav.mainNavWidth = siteNav.$mainNav.outerWidth(); // reset when opening/closing nav to ensure width is detected correctly
-                siteNav.$mobHeader.add(siteNav.$mainNav).removeClass('transition');
-            },
-            beforeEnd: function(){
-                cancelAnimFrame(animRequestId);
-                siteNav.$mobHeader.add(siteNav.$mainNav).addClass('transition'); // add classes here to ensure animation works on MS surface
-            },
-            left: function(){
-                if (!siteNav.$body.hasClass('open-nav')){
-                    siteNav.$body.addClass('open-nav');
-                    siteNav.openNav();
-                }
-            },
-            right: function(){            
-                if (siteNav.$body.hasClass('open-nav')){
-                    siteNav.$body.removeClass('open-nav');
-                    siteNav.closeNav();
-                }
-            },
-            moving: function(d){
-                animRequestId = requestAnimFrame(function(){
-                    siteNav.navMovementPrep(d);
-                });
-            },
-            notReached: function(){
-                if (siteNav.$body.hasClass('open-nav')){
-                    siteNav.openNav(); // reset to open nav position
-                } else {
-                    siteNav.closeNav(); // reset to closed nav position
-                }
-            },
-            reset: function(){
-                siteNav.$mobHeader.add(siteNav.$mainNav).addClass('transition'); // add classes here to reenable animations if user hasn't used custom swipe action
-            }
-        });
-    },
-    
-    setNavScrollOne: function(){ // option 1: use swipe library to override all touch movement in the sidenav
-        var currentTop;
-        siteNav.$mainNav.lpswipe({
-            threshold: 0,
-            swipeDirection: 'vertical',
-            start: function(){
-                if (siteNav.$mainNav[0].scrollHeight > siteNav.$mainNav.outerHeight()){ // detect if the nav is scrollable
-                    siteNav.overrideNavScroll = true;
-                    currentTop = siteNav.$mainNav.scrollTop();
-                }  
-            },
-            moving: function(d){
-                if (siteNav.overrideNavScroll){
-                    siteNav.$mainNav.scrollTop(currentTop - d.posY);
-                }
-            },
-            reset: function(){
-                siteNav.overrideNavScroll = false;
-            }
-        });
-    },
+            });
+        },
 
-    domEvents: function(){
-        var clickEvent = 'ontouchstart' in window ? 'touchend' : 'click';
-        siteNav.$mobOpener.on(clickEvent, function(){ // bind click event for menu icon
-            siteNav.mainNavWidth = siteNav.$mainNav.outerWidth(); // reset when opening/closing nav to ensure width is detected correctly
-            siteNav.$body.toggleClass('open-nav');
-            if (siteNav.$body.hasClass('open-nav')){
-                siteNav.openNav();
-            } else {
-                siteNav.closeNav();
-            }
-        });
-
-        $('#container').on('touchmove', function(e){ // prevent scrolling on the body on webkit mobile devices when nav is open
-            if (siteNav.$body.hasClass('open-nav')){
-                e.preventDefault();
-            }
-        });
-
-        var resizeTimer,
-            orientationchanged = true, 
-            newOrientation,
-            oldOrientation = $(window).width() > $(window).height() ? 'landscape' : 'portrait',
-            isMobile = /android|webos|iphone|ipad|ipod|blackberry|windows phone/i.test(navigator.userAgent.toLowerCase());
-
-        $(window).resize(function(){ // resize event - custom orientation change check for mobile
-            if (resizeTimer){
-                clearTimeout(resizeTimer);
-            }
-            resizeTimer = setTimeout(function(){
-                if (isMobile){
-                    newOrientation = $(window).width() > $(window).height() ? 'landscape' : 'portrait';
-                    orientationchanged = newOrientation == oldOrientation ? false : true;
-                }
-
-                if (orientationchanged){
-                    oldOrientation = newOrientation;
+        setNavScroll: function(){ // use swipe library to override all touch movement in the sidenav
+            var currentTop;
+            lpswipe(siteNav.mainNav, {
+                threshold: 0,
+                swipeDirection: 'vertical',
+                start: function(){
+                    if (siteNav.mainNav.scrollHeight > siteNav.mainNav.offsetHeight){ // detect if the nav is scrollable
+                        siteNav.overrideNavScroll = true;
+                        currentTop = siteNav.mainNav.scrollTop;
+                    }
+                },
+                moving: function(d){
+                    if (siteNav.overrideNavScroll){ // if nav is scrollable, emulate scroll movement by using touch position
+                        siteNav.mainNav.scrollTop = currentTop - d.posY;
+                    }
+                },
+                reset: function(){
                     siteNav.overrideNavScroll = false;
-                    siteNav.mainNavWidth = siteNav.$mainNav.outerWidth(); // reset when resizing in case any styling affects the width
-                    siteNav.$body.removeClass('open-nav');	
-                    siteNav.closeNav();
                 }
-            }, 100);
-        });
+            });
+        },
+
+        domEvents: function(){
+            var clickEvent = 'ontouchstart' in window ? 'touchend' : 'click';
+            if (window.addEventListener){
+                siteNav.mobOpener.addEventListener(clickEvent, function(){
+                    siteNav.mainNavWidth = siteNav.mainNav.offsetWidth; // reset when opening/closing nav to ensure width is detected correctly
+                    if (siteNav.body.className.indexOf('open-nav') > -1){
+                        siteNav.removeClass(siteNav.body, 'open-nav');
+                        siteNav.closeNav();
+                    } else {
+                        siteNav.addClass(siteNav.body, 'open-nav');
+                        siteNav.openNav();
+                    }
+                });
+
+                document.getElementById('container').addEventListener('touchmove', function(e){ // prevent scrolling on the body on webkit mobile devices when nav is open
+                    if (siteNav.body.className.indexOf('open-nav') > -1){
+                        e.preventDefault();
+                    }
+                });
+            }
+        },
+
+        resizeEvent: function(){
+            var resizeTimer,
+                orientationchanged = true,
+                newOrientation,
+                winWidth = window.innerWidth || document.documentElement.offsetWidth,
+                winHeight = window.innerHeight || document.documentElement.offsetHeight,
+                oldOrientation = winWidth > winHeight ? 'landscape' : 'portrait',
+                isMobile = /android|webos|iphone|ipad|ipod|blackberry|windows phone/i.test(navigator.userAgent.toLowerCase());
+
+            function resizeFunc(){ // resize event - custom orientation change check for mobile
+                if (resizeTimer){
+                    clearTimeout(resizeTimer);
+                }
+                resizeTimer = setTimeout(function(){
+                    if (isMobile){
+                        winWidth = window.innerWidth || document.documentElement.offsetWidth,
+                        winHeight = window.innerHeight || document.documentElement.offsetHeight,
+                        newOrientation = winWidth > winHeight ? 'landscape' : 'portrait',
+                        orientationchanged = newOrientation === oldOrientation ? false : true;
+                    }
+
+                    if (orientationchanged){
+                        oldOrientation = newOrientation;
+                        siteNav.overrideNavScroll = false;
+                        siteNav.mainNavWidth = siteNav.mainNav.offsetWidth; // reset when resizing in case any styling affects the width
+                        siteNav.removeClass(siteNav.body, 'open-nav'); // close nav when resizing
+                        siteNav.closeNav();
+                    }
+                }, 100);
+            }
+
+            if (window.addEventListener){
+                window.addEventListener('resize', resizeFunc);
+            } else {
+                window.onresize = resizeFunc();
+            }
+        }
+
+    };
+
+    if (lpswipe !== undefined){
+        lpswipe.sidenav = siteNav;
+        lpswipe.sidenav.init();
     }
-    
-};
+})();
